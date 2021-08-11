8b34f8ca1cc8bbf3363c15c23542a3cbb319947e
holochn <iot@lochnerweb.de>
2021-08-11 20:41:38 +0200

    Added inverted AMP example

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Example sdk_multitasking shows how to run a task on each of the two core
 ## Using FreeRTOS
 Example freertos_single_core runs two tasks on one core (core0). Both tasks use a queue to exchange data.
 ## Using FreeRTOS AND the Raspberry Pi Pico SDK
-Example freertos_multi_core runs one task on core 1 and three tasks (including main) on core0.
+Example freertos_multi_core runs one task on core 1 and three FreeRTOS tasks (including main) on core0.
 The FreeRTOs tasks use a queue to exchange data. One of the FreeRTOs tsks uses SDK's FIFO to exchange data
+with core1.
+## Using FreeRTOS AND the Raspberry Pi Pico SDK (inverted)
+This example is the same as the one above, but with inverted task execution: Core0 runs one SDK task ans Core1 runs three 
+FreeRTOS tasks. The FreeRTOs tasks use a queue to exchange data. One of the FreeRTOs tsks uses SDK's FIFO to exchange data
 with core1.
---
+++ freertos_multi_core_inverted/main.cpp
@@ -0,0 +1,123 @@
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+#include <iostream>
+#include "pico/multicore.h"
+#include "pico/stdlib.h"
+
+TaskHandle_t tskHdnl_evGen          = nullptr;
+TaskHandle_t tskHdnl_stateManager   = nullptr;
+QueueHandle_t eventQueue            = nullptr;
+
+const uint8_t EVENT_PIN_HIGH = 1;
+const uint8_t EVENT_PIN_LOW  = 0;
+
+
+void taskOnCore0(void)
+{
+    int pin = 14;
+    uint32_t fromCore0 = 0;
+    
+    gpio_init(pin);
+    gpio_set_dir(pin, GPIO_OUT);
+    while (true) {
+        std::cout << ".\n";
+        gpio_put(pin, 1);
+        sleep_ms( 200 );
+        gpio_put(pin, 0);
+        sleep_ms( 200 );
+
+        if(multicore_fifo_rvalid() == true)
+        {
+            fromCore0 = multicore_fifo_pop_blocking ();
+            std::cout << "Message from core 0: " << int(fromCore0) << "\n";
+            
+            fromCore0 += 10;
+            std::cout << "Sending to core 0 " << int(fromCore0) << "\n";
+            multicore_fifo_push_blocking(fromCore0);
+        }
+    }
+}
+
+void eventGenerator(void *p)
+{
+    const int pin = 15;
+    gpio_init(pin);
+    gpio_set_dir(pin, GPIO_OUT);
+    while (true) {
+        gpio_put(pin, 1);
+        xQueueSend(eventQueue, &EVENT_PIN_HIGH, 0);
+        vTaskDelay( 200/portTICK_PERIOD_MS );
+
+    
+        gpio_put(pin, 0);
+        xQueueSend(eventQueue, &EVENT_PIN_LOW, 0);
+        vTaskDelay( 100/portTICK_PERIOD_MS );
+    }
+}
+
+void stateManager(void *p)
+{
+    uint8_t i = 255;
+    uint32_t fromCore1 = -255;
+
+    multicore_fifo_push_blocking(fromCore1);
+    
+    while(true)
+    {
+        if( xQueueReceive(eventQueue, &i, portMAX_DELAY) == pdPASS )
+        {
+            std::cout << "received " << i << std::endl;
+            switch( i)
+            {
+                case EVENT_PIN_HIGH: std::cout << "Pin is high\n";
+                                    break;
+                case EVENT_PIN_LOW : std::cout << "Pin is low\n";
+                                    break;
+                default: std::cout << "Unkown state\n";
+                                    break;
+            }
+        }
+
+        if(multicore_fifo_rvalid() == true)
+        {
+            fromCore1 = multicore_fifo_pop_blocking ();
+            std::cout << "Message from core 1: " << int(fromCore1) << "\n";
+            
+            fromCore1 += 10;
+            std::cout << "Sending to core 1 " << int(fromCore1) << "\n";
+            multicore_fifo_push_blocking(fromCore1);
+        }
+    }
+}
+
+void freeRTOStask(void)
+{
+    eventQueue = xQueueCreate(5, sizeof(int) );
+
+    xTaskCreate(eventGenerator, "eventGenerator", 256, nullptr, 1, &tskHdnl_evGen);
+    xTaskCreate(stateManager, "stateManager", 256, nullptr, 1, &tskHdnl_stateManager);
+
+    vTaskStartScheduler();
+
+    while (true)
+    {
+        // should only get here if there is insufficient RAM
+    }
+
+}
+
+int main()
+{
+    stdio_init_all();
+    std::cout << "Start main app\n";
+
+    multicore_launch_core1(freeRTOStask);
+
+    taskOnCore0();
+
+    while (true)
+    {
+        // should never get here
+    }
+}
